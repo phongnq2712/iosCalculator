ad35c0eb51c891e7c45fc87bbe3fdaa955c87c12
phongngo <phongnq2712@gmail.com>
2021-09-23 23:46:21 +0700

    add internal document for View Controller

--- calculatorSimulator/ViewController.swift
+++ calculatorSimulator/ViewController.swift
@@ -1,9 +1,11 @@
 //
-//  ViewController.swift
-//  calculatorSimulator
+// calculatorSimulator
 //
-//  Created by Phong on 11/09/2021.
-//
+// Created by Phong on 11/09/2021.
+// File Name:   ViewController.swift
+// Author:      Quoc Phong Ngo
+// Student ID:  301148406
+// Version 1.0
 
 import UIKit
 
